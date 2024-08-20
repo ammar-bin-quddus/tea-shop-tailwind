0f21eec735565d9bf5205de2b3b7609adc9cf9f4
ammar-bin-quddus <ammarcghs372@gmail.com>
2024-08-20 16:36:59 +0600

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+        clifford: '#da373d',
+      },
+      fontFamily: {
+        'manrope': ["Manrope", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
+
